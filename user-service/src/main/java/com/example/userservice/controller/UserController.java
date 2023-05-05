facfef02b4115142697222347df309ec7dba78e7
youngmin Moon <>
2023-05-05 23:40:09 +0900

    1. 사용자 추가 jpa 2 듣는중

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,19 +1,22 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class UserController {
 
-//    private final Environment env;
+    //    private final Environment env;
     private final Greeting greeting;
+    private final UserService userService;
 
     @GetMapping("health_check")
     public String status() {
@@ -24,5 +27,14 @@ public class UserController {
     public String welcome() {
 //        return env.getProperty("greeting.message");
         return greeting.getMessage();
+    }
+
+    @PostMapping("users")
+    public String createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+        return "Create user method is called";
     }
 }

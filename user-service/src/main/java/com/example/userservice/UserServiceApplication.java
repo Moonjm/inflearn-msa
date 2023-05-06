8bc1f1334d106e6e598bc24f4aa5bb2cf3f48d7b
youngmin Moon <>
2023-05-06 22:23:13 +0900

    1. spring security 추가

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,6 +3,13 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +19,12 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("bcrypt", encoder);
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
+    }
 }
--- user-service/src/main/java/com/example/userservice/security/WebSecurityConfig.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurityConfig.java
@@ -1,2 +1,24 @@
-package com.example.userservice.security;public class WebSecurityConfig {
+package com.example.userservice.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/users/**").permitAll()
+                .requestMatchers(PathRequest.toH2Console()).permitAll());
+        return http.build();
+
+    }
 }
